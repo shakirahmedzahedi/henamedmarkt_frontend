a9d07525d80b75211bf2ef57011e1b75767467ad
Shakir Ahmed Zahedi <shakirahmed84@yahoo.com>
2025-06-15 10:46:13 +0200

    add same GMT

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './reducer/Store';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
 const container = document.getElementById('root');
